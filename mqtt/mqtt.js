5cd59746727c8891d42b22a163b3146a7aa24183
Sanjiv1458 <sanjiv1458@gmail.com>
2023-03-21 02:27:33 +0530

    #

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -10,16 +10,16 @@ const app = express();
 app.use(express.static('public'));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 const port = 5001;
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
@@ -29,9 +29,9 @@ const lat = coordsArray[0];
 const lon = coordsArray[1];
 
 
-client.on('connect', () => { 
-    client.subscribe('/sensorData');
-    console.log('mqtt connected');
+client.on('connect', () => {
+  client.subscribe('/sensorData');
+  console.log('mqtt connected');
 });
 
 client.on('message', (topic, message) => {
@@ -41,56 +41,46 @@ client.on('message', (topic, message) => {
   }
 });
 
-client.on('message', (topic, message) => {
-    if (topic == '/sensorData') {
-      const data = JSON.parse(message);
-  
-      Device.findOne({"name": data.deviceId }, (err, device) => {
-        if (err) {
-          console.log(err)
-        }
-  
-        const { sensorData } = device;
-        const { ts, loc, temp } = data;
-  
-        sensorData.push({ ts, loc, temp });
-        device.sensorData = sensorData;
-  
-        device.save(err => {
-          if (err) {
-            console.log(err)
-          }
-        });
-      });
-    }
-  });
+
+client.on('message', async (topic, message) => {
+  if (topic == '/sensorData') {
+    const data = JSON.parse(message);
+
+    let device = await Device.findOne({ "name": data.deviceId });
+    const { sensorData } = device;
+    const { ts, loc, temp } = data;
+    sensorData.push({ ts, loc, temp });
+    device.sensorData = sensorData;
+    device.save();
+  }
+});
 
 app.post('/send-command', (req, res) => {
-    const { deviceId, command }  = req.body;
-    const topic = `/myid/command/${deviceId}`;
-    client.publish(topic, command, () => {
-      res.send('published new message');
-    });
+  const { deviceId, command } = req.body;
+  const topic = `/myid/command/${deviceId}`;
+  client.publish(topic, command, () => {
+    res.send('published new message');
   });
+});
 
-  app.put('/sensor-data', (req, res) => {
-    const { deviceId }  = req.body;
-  
-    const [lat, lon] = randomCoordinates().split(", ");
-    const ts = new Date().getTime();
-    const loc = { lat, lon };
-    min = Math.ceil(20);
-    max = Math.floor(50);
-    temp = Math.floor(Math.random() * (max - min + 1) + min);
-  
-    const topic = `/sensorData`;
-    const message = JSON.stringify({ deviceId, ts, loc, temp });
-  
-    client.publish(topic, message, () => {
-      res.send('published new message');
-    });
+app.put('/sensor-data', (req, res) => {
+  const { deviceId } = req.body;
+
+  const [lat, lon] = randomCoordinates().split(", ");
+  const ts = new Date().getTime();
+  const loc = { lat, lon };
+  min = Math.ceil(20);
+  max = Math.floor(50);
+  temp = Math.floor(Math.random() * (max - min + 1) + min);
+
+  const topic = `/sensorData`;
+  const message = JSON.stringify({ deviceId, ts, loc, temp });
+
+  client.publish(topic, message, () => {
+    res.send('published new message');
   });
-  
-app.listen(port, () => { 
-    console.log(`listening on port ${port}`);
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
